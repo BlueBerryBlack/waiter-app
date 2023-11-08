b44f90e91af16521d1414df164412e51dd37d4e5
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-11-08 11:28:01 +0100

    basic components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
+import Table from './components/Table/Table';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer';
+
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
 const App = () => {
   return (
-    <div>
-  
-    </div>
+    <main>
+    <Container>
+    <NavBar/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/table/:tableId" element={<Table />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      
+      <Footer />
+    </Container>
+    </main>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+
+        <div >  
+            <h5>footer</h5>       
+        </div>
+        
+    );
+};
+export default Footer;
+
