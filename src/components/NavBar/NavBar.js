a84e764157821918cb8b1b7accad594aa67960c1
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-11-08 14:01:49 +0100

    navBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,15 @@
+import Nav from 'react-bootstrap/Nav';
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.scss';
+
 const NavBar = () => {
     return (
-
-        <div >       
-            <h1>NavBar</h1>  
+        <div className={styles.background}>
+            <Nav className={`${styles.navigation} me-auto`}>
+                <Nav.Link as={NavLink} to="/" className={styles.link}>Waiter.app</Nav.Link>
+                <Nav.Link as={NavLink} to="/" className={styles.link}>Home</Nav.Link>
+            </Nav>
         </div>
-        
     );
 };
 export default NavBar;
